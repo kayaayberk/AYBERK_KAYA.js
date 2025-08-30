13a6e1cc6586e1004dfe9d9615092d1cc6679c10
Ayberk <kayaayberk98@gmail.com>
2025-08-31 01:32:30 +0300

    feat: add base constants and lifecycle state for carousel

--- AYBERK_KAYA.js
+++ AYBERK_KAYA.js
@@ -11,6 +11,24 @@
 
   withjQuery((jQuery) => {
     const $ = jQuery;
-    
+    const ANCHOR_SELECTOR = "body > eb-root > cx-storefront > main > cx-page-layout > cx-page-slot.Section1.has-components";
+    const API_URL = "https://gist.githubusercontent.com/sevindi/8bcbde9f02c1d4abe112809c974e1f49/raw/9bf93b58df623a9b16f1db721cd0a7a539296cf0/products.json";
+
+    const ROOT_CLASS = "ebk-carousel";
+    const PRODUCTS_KEY = "ebk:products";
+    const FAVORITES_KEY = "ebk:favorites";
+    const TITLE_TEXT = "Beğenebileceğinizi düşündüklerimiz";
+    const STYLE_VARIANT = "website";
+
+    // Drag/snap feel
+    const DRAG_THRESHOLD_PX = 3;
+    const ALIGN_EPSILON_PX = 1.5;
+
+    // Lifecycle tokens
+    let navToken = 0;
+    let pendingObserver = null;
+    let pendingFetchController = null;
+    let didLogWrongPage = false;
+    let locationWatcherInstalled = false;
   });
 })();
